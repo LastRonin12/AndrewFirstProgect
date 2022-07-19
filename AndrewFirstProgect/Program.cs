2f969e69c77b65b5f41d256db78efb57d82bc3c3
nsu <nsu@WS4210-16>
2022-07-19 15:33:42 +0700

    Добавьте файлы проекта.

---
+++ AndrewFirstProgect/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AndrewFirstProgect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programm started again");
+            int n = 6;
+            Console.WriteLine($"n={n}");
+            int[] ar = { 1,2,3,4,5,6 };
+            for (int i = 0; i < n; i++)
+            {
+                if (ar[i]% 2 == 0)
+                {
+                    Console.WriteLine($"ar={ar[i]}");
+                }                
+
+            }
+        }
+    }
+}
