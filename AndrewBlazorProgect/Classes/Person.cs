4eb85384d23fa7e3748939c8c4504cc35d03de4d
nsu <nsu@WS4210-16>
2022-07-26 14:53:12 +0700

    gouygy

---
+++ AndrewBlazorProgect/Classes/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndrewBlazorProgect.Classes
+{
+    public class Person
+    {
+        public string nickname;
+        public string password;
+
+    }
+}
